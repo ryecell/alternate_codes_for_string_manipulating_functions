38709daf828dd0f837248a71cb60feb61354757c
unknown <janruzzellorquia88@gmail.com>
2025-04-01 22:04:15 +0800

    first occurence from last character program without using rindex() function

--- first_occurence_from_last_character.py
+++ first_occurence_from_last_character.py
@@ -1,7 +1,16 @@
+#ask user for inputs
+user_string = input("Enter your string: ")
+sub_string_to_find = input("Enter substring to locate first occurence of: ")
 
 #tweaking some of the code used for index() function to start from the last character
-def find_rindex(s, sub):
-    for i in range(len(s) - len(sub), -1, -1):
-        if s[i:i+len(sub)] == sub:
-            return i
+def find_rindex(string, substring):
+    for ind in range(len(string) - len(substring), -1, -1):
+        if string[ind:ind+len(substring)] == substring:
+            return ind
     return -1
+
+index = find_rindex(user_string, sub_string_to_find)
+if index != -1:
+    print(f"The substring '{sub_string_to_find}' first appears at index {index} in the string.")
+else:
+    print(f"The substring '{sub_string_to_find}' was not found in the string.")
