754ce0999619a1874c76eb462361055d6f5a3be8
unknown <janruzzellorquia88@gmail.com>
2025-04-01 20:42:49 +0800

    rstrip program without using rstrip() function

--- rstrip_function_code.py
+++ rstrip_function_code.py
@@ -1,8 +1,11 @@
-
+#ask user for input
+user_input = input("Enter your string with multiple spaces at the back: ")
 
 #rstrip function
-def remove_trailing_spaces(s):
-    index = len(s) - 1
-    while index >= 0 and s[index] == ' ':
+def remove_trailing_spaces(string):
+    index = len(string) - 1
+    while index >= 0 and string[index] == ' ':
         index -= 1
-    return s[:index + 1]
+    return string[:index + 1]
+
+print(remove_trailing_spaces(user_input))
