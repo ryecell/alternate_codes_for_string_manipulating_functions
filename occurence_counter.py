770f06d8bc135d464c36402d43c8c67a02c9ff3e
unknown <janruzzellorquia88@gmail.com>
2025-04-01 21:21:54 +0800

    occurence counter without using count() function

--- occurence_counter.py
+++ occurence_counter.py
@@ -1,7 +1,13 @@
+#ask user for string input and string to count
+string_input = input("Enter your string: ")
+string_to_count = input("Enter substring you want to count: ")
+
 #custom counter function
-def count_occurrences(s, sub):
+def count_occurrences(string, sub_string):
     count = 0
-    for i in range(len(s) - len(sub) + 1):
-        if s[i:i+len(sub)] == sub:
+    for char in range(len(string) - len(sub_string) + 1):
+        if string[char:char+len(sub_string)] == sub_string:
             count += 1
     return count
+
+print(count_occurrences(string_input,string_to_count))
