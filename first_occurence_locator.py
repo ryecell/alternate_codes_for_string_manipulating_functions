281636778ef1b53f7549276f5f1b35f0a6fc4838
unknown <janruzzellorquia88@gmail.com>
2025-04-01 21:58:44 +0800

    first occurence finder without using index() function

--- first_occurence_locator.py
+++ first_occurence_locator.py
@@ -1,6 +1,16 @@
+#ask user for inputs
+user_string = input("Enter your string: ")
+sub_string_to_find = input("Enter substring to locate first occurence of: ")
+
 #custom index function
 def find_index(string, sub_string):
     for ind in range(len(string) - len(sub_string) + 1):
         if string[ind:ind+len(sub_string)] == sub_string:
             return ind
     return -1
+
+index = find_index(user_string, sub_string_to_find)
+if index != -1:
+    print(f"The substring '{sub_string_to_find}' first appears at index {index} in the string.")
+else:
+    print(f"The substring '{sub_string_to_find}' was not found in the string.")
