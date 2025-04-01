21cca6f8def6f1ae54a9510455dafd5559ba8ce4
unknown <janruzzellorquia88@gmail.com>
2025-04-01 21:16:14 +0800

    occurence counter without using counter() function

---
+++ occurence_counter.py
@@ -0,0 +1,7 @@
+#custom counter function
+def count_occurrences(s, sub):
+    count = 0
+    for i in range(len(s) - len(sub) + 1):
+        if s[i:i+len(sub)] == sub:
+            count += 1
+    return count
